39a74200aa153b84344e3b8cc51ff4710b5f540d
Mastoast <dgekec@gmail.com>
2023-06-17 10:41:08 +0200

    Add random for test

--- scripts/test_scene.gd
+++ scripts/test_scene.gd
@@ -4,11 +4,11 @@ const qte := preload("res://objects/qte.tscn")
 var inputs = ["ui_left", "ui_right", "ui_up", "ui_down"]
 
 func _ready():
-	pass
+	randomize()
 
 func _process(delta):
 	if get_child_count() < 2:
-		spawn_qte(1.0, Vector2(200, 200))
+		spawn_qte(randf_range(0.5, 1.2), Vector2(200, 200))
 
 func spawn_qte(timer, position):
 	var new_qte = qte.instantiate()
