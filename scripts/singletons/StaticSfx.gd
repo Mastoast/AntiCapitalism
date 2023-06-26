cbdaa2c0923f082d8af7a068e6191832df6853b8
Mastoast <dgekec@gmail.com>
2023-06-27 01:10:39 +0200

    Add sfx player

---
+++ scripts/singletons/StaticSfx.gd
@@ -0,0 +1,22 @@
+extends Node
+
+#var test_sfx = load("res://sounds/test.wav")
+
+var audio_players = []
+
+func _ready():
+	pass
+
+func _process(delta):
+	for player in audio_players:
+		if !player.playing:
+			player.queue_free()
+			audio_players.erase(player)
+
+func play_sfx(stream, pitch = 1.0, position = 0.0):
+	var player = AudioStreamPlayer.new()
+	player.stream = stream
+	player.pitch_scale = pitch
+	get_tree().current_scene.add_child(player)
+	player.play(position)
+	audio_players.append(player)
