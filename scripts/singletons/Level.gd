3d8d2d38920c056422ca6e84deeaf6fadbf40f23
Becher <mbelkhenchir@gmail.com>
2023-06-26 22:00:22 +0200

    tweak part 2

--- scripts/singletons/Level.gd
+++ scripts/singletons/Level.gd
@@ -2,8 +2,8 @@ extends Node
 
 var level1 = {
 	"music": StaticMusic.music1, "pitch": 1.0,
-	"combo_bar_start": 80.0, "combo_decrease_speed" : 1.0,
-	"threshold_survival_time" : 5.0, "combo_threshold" : 20.0,
+	"combo_bar_start": 100.0, "combo_decrease_speed" : 1.2,
+	"threshold_survival_time" : 3.0, "combo_threshold" : 20.0,
 	"briefs" : [
 	"""[font_size=44][center]02:00 AM
 
@@ -20,7 +20,7 @@ Night {night_count}[/center][/font_size]""",
 
 var level2 = {
 	"music": StaticMusic.music1, "pitch": 1.1,
-	"combo_bar_start": 50.0, "combo_decrease_speed" : 1.2,
+	"combo_bar_start": 100.0, "combo_decrease_speed" : 1.2,
 	"threshold_survival_time" : 3.0, "combo_threshold" : 40.0,
 	"briefs" : [
 		"""[font_size=44][center]02:00 AM
@@ -37,21 +37,19 @@ Night {night_count}[/center][/font_size]"""
 
 var level3 = {
 	"music": StaticMusic.music1, "pitch": 1.2,
-	"combo_bar_start": 80.0, "combo_decrease_speed" : 1.5,
+	"combo_bar_start": 100.0, "combo_decrease_speed" : 1.2,
 	"threshold_survival_time" : 3.0, "combo_threshold" : 60.0,
 	"briefs" : [
-		"""[font_size=44][center]02:00 AM
-
-Night {night_count}[/center][/font_size]"""
+		"""[font_size=44][center]02:00 AM\n\nNight {night_count}[/center][/font_size]"""
 	],
 	"trashes" : [
 	{"distance": 1.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
-	{"distance": 2.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern2},
-	{"distance": 3.0, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern1},
+	{"distance": 1.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern2},
+	{"distance": 2.5, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern1},
 	{"distance": 4.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
-	{"distance": 5.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
-	{"distance": 7.0, "sprite": Color.DARK_RED, "pattern": Pattern.pattern1},
-	{"distance": 8.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
+	{"distance": 4.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
+	{"distance": 5.0, "sprite": Color.DARK_RED, "pattern": Pattern.pattern1},
+	{"distance": 6.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
 ]}
 
 var capitalism_levels = [level1, level2, level3]
