5cc340fbb9958f0afe85032852a50a31419d8f06
Becher <mbelkhenchir@gmail.com>
2023-06-26 21:21:54 +0200

    some tweak on level

--- scripts/singletons/Level.gd
+++ scripts/singletons/Level.gd
@@ -2,7 +2,7 @@ extends Node
 
 var level1 = {
 	"music": StaticMusic.music1, "pitch": 1.0,
-	"combo_bar_start": 100.0, "combo_decrease_speed" : 1.0,
+	"combo_bar_start": 80.0, "combo_decrease_speed" : 1.0,
 	"threshold_survival_time" : 5.0, "combo_threshold" : 20.0,
 	"briefs" : [
 	"""[font_size=44][center]02:00 AM
@@ -15,13 +15,12 @@ Night {night_count}[/center][/font_size]""",
 	{"distance": 2.0, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern1},
 	{"distance": 3.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern2},
 	{"distance": 3.5, "sprite": Color.BLUE_VIOLET, "pattern": Pattern.pattern1},
-	{"distance": 4.5, "sprite": Color.DARK_RED, "pattern": Pattern.pattern1},
-	{"distance": 5.5, "sprite": Color.DARK_GREEN, "pattern": Pattern.pattern2},
+	{"distance": 4.5, "sprite": Color.DARK_RED, "pattern": Pattern.pattern2},
 ]}
 
 var level2 = {
 	"music": StaticMusic.music1, "pitch": 1.1,
-	"combo_bar_start": 80.0, "combo_decrease_speed" : 1.0,
+	"combo_bar_start": 50.0, "combo_decrease_speed" : 1.2,
 	"threshold_survival_time" : 3.0, "combo_threshold" : 40.0,
 	"briefs" : [
 		"""[font_size=44][center]02:00 AM
@@ -30,16 +29,15 @@ Night {night_count}[/center][/font_size]"""
 	],
 	"trashes" : [
 	{"distance": 1.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern1},
-	{"distance": 2.0, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern1},
-	{"distance": 3.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern2},
+	{"distance": 2.0, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern2},
+	{"distance": 3.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern1},
 	{"distance": 3.5, "sprite": Color.BLUE_VIOLET, "pattern": Pattern.pattern2},
-	{"distance": 4.0, "sprite": Color.DARK_RED, "pattern": Pattern.pattern1},
-	{"distance": 5.0, "sprite": Color.DARK_GREEN, "pattern": Pattern.pattern3},
+	{"distance": 4.0, "sprite": Color.DARK_GREEN, "pattern": Pattern.pattern3},
 ]}
 
 var level3 = {
 	"music": StaticMusic.music1, "pitch": 1.2,
-	"combo_bar_start": 30.0, "combo_decrease_speed" : 1.0,
+	"combo_bar_start": 80.0, "combo_decrease_speed" : 1.5,
 	"threshold_survival_time" : 3.0, "combo_threshold" : 60.0,
 	"briefs" : [
 		"""[font_size=44][center]02:00 AM
@@ -49,12 +47,11 @@ Night {night_count}[/center][/font_size]"""
 	"trashes" : [
 	{"distance": 1.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
 	{"distance": 2.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern2},
-	{"distance": 3.0, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern2},
-	{"distance": 3.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
-	{"distance": 5.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
-	{"distance": 6.5, "sprite": Color.DARK_RED, "pattern": Pattern.pattern1},
+	{"distance": 3.0, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern1},
+	{"distance": 4.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
+	{"distance": 5.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
 	{"distance": 7.0, "sprite": Color.DARK_RED, "pattern": Pattern.pattern1},
-	{"distance": 7.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
+	{"distance": 8.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
 ]}
 
 var capitalism_levels = [level1, level2, level3]
