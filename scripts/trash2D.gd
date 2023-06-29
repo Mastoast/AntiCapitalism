8a0d3a93bca63645185d8e2f50174cdc2138eaf4
Becher <mbelkhenchir@gmail.com>
2023-06-30 01:10:27 +0200

    Anim on trash

--- scripts/trash2D.gd
+++ scripts/trash2D.gd
@@ -8,9 +8,11 @@ var coords:Vector2i
 func init(pattern, coords):
 	self.pattern = pattern
 	self.coords = coords
+	StaticMusic.new_beat.connect(_start_anim)
 
-func _ready():
-	pass
+func _start_anim():
+	StaticMusic.new_beat.disconnect(_start_anim)	
+	$Sprite2D/AnimationPlayer.play("beat", -1.0, 1.0 / StaticMusic.beat_length, false)
 
 func _process(delta):
 	pass
