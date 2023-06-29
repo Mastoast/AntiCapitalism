c221db2c60562f85e63cc246b4f72b7116af4c04
Mastoast <dgekec@gmail.com>
2023-06-29 18:46:08 +0200

    Music2 + fix pattern line

--- scripts/pattern_player.gd
+++ scripts/pattern_player.gd
@@ -25,9 +25,10 @@ var last_qte
 var qte_count = 0
 var current_pattern_line_center
 var nb_fail_accepted
+var pattern_drawing_center
 
 # drawing pattern
-@export var line_beat_length = 40.0
+@export var line_beat_length = 50.0
 @onready var pattern_line:Line2D = $PatternLine
 
 func _ready():
@@ -36,13 +37,14 @@ func _ready():
 	start_pattern(Pattern.pattern1)
 	# DEBUG
 
-func start_pattern(pattern):
+func start_pattern(pattern, drawing_center = Vector2(0, 0)):
 	qte_count = 0
 	last_qte = null
 	nb_fail_accepted = pattern["nb_fail_accepted"]
 	print("nb fail accepted : " + str(nb_fail_accepted))
 	buffer_qte = pattern["pattern"].duplicate(true)
 	pattern_start = StaticMusic.get_player_total_position()
+	pattern_drawing_center = drawing_center
 	create_pattern_drawing()
 	sort_qte()
 
@@ -59,6 +61,7 @@ func stop_current_pattern():
 	for qte in get_tree().get_nodes_in_group("qte"):
 		qte.canceled()
 	buffer_qte.clear()
+	pattern_line.visible = false
 
 func _process(delta):
 	spawn_qte_on_time()
@@ -73,7 +76,7 @@ func _input(event):
 		if (qte.timer - qte.counter < min_time):
 			selected_qte = qte
 			min_time = qte.timer - qte.counter
-			
+
 	if selected_qte:
 		var result = selected_qte.try_input(event, nb_fail_accepted <= 0)
 		if !result : 
@@ -130,6 +133,7 @@ func _on_qte_success(data, precision:float):
 		pattern_next_anim.emit(data["anim_sprite"])
 	qte_count -= 1
 	if buffer_qte.is_empty() and qte_count == 0:
+		pattern_line.visible = false
 		pattern_succeeded.emit()
 
 func _on_qte_failure(data, precision:float):
@@ -142,16 +146,14 @@ func _on_bad_input():
 
 func create_pattern_drawing():
 	pattern_line.clear_points()
+	pattern_line.visible = true
 	var last_direction = Vector2.ZERO
 	pattern_line.add_point(Vector2.ZERO)
-	#print(buffer_qte)
-	#print(expected_actions[buffer_qte[0]["input"]])
 	var last_position = pattern_line.get_point_position(pattern_line.get_point_count()-1) + expected_actions[buffer_qte[0]["input"]] * line_beat_length * buffer_qte[0]["delay"]
 	pattern_line.add_point(last_position)
 	line_length = Vector2.ZERO.distance_to(last_position)
 	var pattern_length = 0
 	for i in range(buffer_qte.size()):
-		#print("last position: ", last_position)
 		pattern_length += buffer_qte[i]["delay"]
 		var used_direction
 		var new_vector
@@ -165,16 +167,23 @@ func create_pattern_drawing():
 		else:
 			new_vector = buffer_qte[i]["delay"] * used_direction * line_beat_length
 		var new_position = last_position + new_vector
-		line_length += last_position.distance_to(new_position)
+		if i < buffer_qte.size()-1:
+			line_length += last_position.distance_to(new_position)
 		pattern_line.add_point(new_position)
 		last_position = new_position
 	pattern_end = pattern_start + pattern_length * StaticMusic.beat_length
-	print(pattern_start, ' ', pattern_end, ' ', line_length,' ', StaticMusic.beat_length)
+	# Draw points
+	for item in pattern_line.points:
+		var new = $Icon.duplicate()
+		pattern_line.add_child(new)
+		new.position = item
 
 func update_pattern_drawing():
-	if !buffer_qte.is_empty():
-		var pattern_ratio = minf(100.0, (StaticMusic.get_player_total_position() - pattern_start) / (pattern_end - pattern_start))
-		pattern_line.position = Vector2(300, 300) + get_position_on_line_from_ratio(pattern_ratio)
+	if !buffer_qte.is_empty() or qte_count > 0:
+		var pattern_ratio = minf(1.1, (StaticMusic.get_player_total_position() - pattern_start) / (pattern_end - pattern_start))
+		var new_position = pattern_drawing_center + get_position_on_line_from_ratio(pattern_ratio)
+		if new_position:
+			pattern_line.position = new_position
 
 func get_position_on_line_from_ratio(ratio):
 	var current_length = line_length * ratio
@@ -182,9 +191,6 @@ func get_position_on_line_from_ratio(ratio):
 	for i in range(1, pattern_line.get_point_count()):
 		var current_segment_length = pattern_line.get_point_position(i-1).distance_to(pattern_line.get_point_position(i))
 		if incr_length + current_segment_length >= current_length:
-
 			return -lerp(pattern_line.get_point_position(i-1), pattern_line.get_point_position(i), (current_length - incr_length) / current_segment_length)
 		else:
 			incr_length += current_segment_length
-	return Vector2.ZERO
-	# TODO clean
