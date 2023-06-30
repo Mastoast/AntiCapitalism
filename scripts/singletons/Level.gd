a172ce0366a4bafe94dc21e1c52848057f305dfd
Becher <mbelkhenchir@gmail.com>
2023-06-30 11:40:16 +0200

    add sprite for all trashes

--- scripts/singletons/Level.gd
+++ scripts/singletons/Level.gd
@@ -1,7 +1,10 @@
 extends Node
 
+const old_container := preload("res://sprites/Cap/OldContainer.png")
 const container_texture := preload("res://sprites/Cap/Container.png")
+const big_container_texture := preload("res://sprites/Cap/BigContainer.png")
 const trash_bag_texture := preload("res://sprites/Cap/TrashBag.png")
+const trash_bag_pine := preload("res://sprites/Cap/TrashPile.png")
 const open_trash_bag_texture := preload("res://sprites/Cap/OpenTrashBag.png")
 
 var level1 = {
@@ -31,11 +34,11 @@ var level2 = {
 Night {night_count}[/center][/font_size]"""
 	],
 	"trashes" : [
-	{"distance": 0.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern1},
-	{"distance": 1.0, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern3},
-	{"distance": 1.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern1},
-	{"distance": 1.75, "sprite": Color.BLUE_VIOLET, "pattern": Pattern.pattern2},
-	{"distance": 2.0, "sprite": Color.DARK_GREEN, "pattern": Pattern.pattern4},
+	{"distance": 0.5, "sprite": trash_bag_texture, "pattern": Pattern.pattern1},
+	{"distance": 1.0, "sprite": container_texture, "pattern": Pattern.pattern3},
+	{"distance": 1.5, "sprite": trash_bag_texture, "pattern": Pattern.pattern1},
+	{"distance": 1.75, "sprite": open_trash_bag_texture, "pattern": Pattern.pattern2},
+	{"distance": 2.0, "sprite": old_container, "pattern": Pattern.pattern4},
 ]}
 
 var level3 = {
@@ -46,13 +49,13 @@ var level3 = {
 		"""[font_size=44][center]02:00 AM\n\nNight {night_count}[/center][/font_size]"""
 	],
 	"trashes" : [
-	{"distance": 0.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern4},
-	{"distance": 0.75, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern2},
-	{"distance": 1.25, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern1},
-	{"distance": 1.75, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
-	{"distance": 2.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern4},
-	{"distance": 2.25, "sprite": Color.DARK_RED, "pattern": Pattern.pattern1},
-	{"distance": 3.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern4},
+	{"distance": 0.5, "sprite": old_container, "pattern": Pattern.pattern4},
+	{"distance": 0.75, "sprite": open_trash_bag_texture, "pattern": Pattern.pattern2},
+	{"distance": 1.25, "sprite": trash_bag_texture, "pattern": Pattern.pattern1},
+	{"distance": 1.75, "sprite": container_texture, "pattern": Pattern.pattern3},
+	{"distance": 2.0, "sprite": old_container, "pattern": Pattern.pattern4},
+	{"distance": 2.25, "sprite": trash_bag_texture, "pattern": Pattern.pattern1},
+	{"distance": 3.0, "sprite": old_container, "pattern": Pattern.pattern4},
 ]}
 
 var level4 = {
@@ -63,13 +66,13 @@ var level4 = {
 		"""[font_size=44][center]02:00 AM\n\nNight {night_count}[/center][/font_size]"""
 	],
 	"trashes" : [
-	{"distance": 0.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern4},
-	{"distance": 0.75, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern3},
-	{"distance": 1.25, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern4},
-	{"distance": 1.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern5},
-	{"distance": 2.25, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern2},
-	{"distance": 3.0, "sprite": Color.DARK_RED, "pattern": Pattern.pattern4},
-	{"distance": 3.25, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern6},
+	{"distance": 0.5, "sprite": old_container, "pattern": Pattern.pattern4},
+	{"distance": 0.75, "sprite": container_texture, "pattern": Pattern.pattern3},
+	{"distance": 1.25, "sprite": old_container, "pattern": Pattern.pattern4},
+	{"distance": 1.5, "sprite": trash_bag_pine, "pattern": Pattern.pattern5},
+	{"distance": 2.25, "sprite": open_trash_bag_texture, "pattern": Pattern.pattern2},
+	{"distance": 3.0, "sprite": old_container, "pattern": Pattern.pattern4},
+	{"distance": 3.25, "sprite": big_container_texture, "pattern": Pattern.pattern6},
 ]}
 var level5 = {
 	"music": StaticMusic.music1, "pitch": 1.2,
@@ -79,13 +82,13 @@ var level5 = {
 		"""[font_size=44][center]02:00 AM\n\nNight {night_count}[/center][/font_size]"""
 	],
 	"trashes" : [
-	{"distance": 0.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern5},
-	{"distance": 0.75, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern6},
-	{"distance": 1.25, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern4},
-	{"distance": 1.75, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern5},
-	{"distance": 2.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern6},
-	{"distance": 2.25, "sprite": Color.DARK_RED, "pattern": Pattern.pattern5},
-	{"distance": 3.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern6},
+	{"distance": 0.5, "sprite": trash_bag_pine, "pattern": Pattern.pattern5},
+	{"distance": 0.75, "sprite": big_container_texture, "pattern": Pattern.pattern6},
+	{"distance": 1.25, "sprite": old_container, "pattern": Pattern.pattern4},
+	{"distance": 1.75, "sprite": trash_bag_pine, "pattern": Pattern.pattern5},
+	{"distance": 2.0, "sprite": big_container_texture, "pattern": Pattern.pattern6},
+	{"distance": 2.25, "sprite": trash_bag_pine, "pattern": Pattern.pattern5},
+	{"distance": 3.0, "sprite": big_container_texture, "pattern": Pattern.pattern6},
 ]}
 
 var level6 = {
@@ -96,13 +99,13 @@ var level6 = {
 		"""[font_size=44][center]02:00 AM\n\nNight {night_count}[/center][/font_size]"""
 	],
 	"trashes" : [
-	{"distance": 0.5, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern5},
-	{"distance": 0.75, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern6},
-	{"distance": 1.25, "sprite": Color.MIDNIGHT_BLUE, "pattern": Pattern.pattern4},
-	{"distance": 1.75, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern5},
-	{"distance": 2.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern6},
-	{"distance": 2.25, "sprite": Color.DARK_RED, "pattern": Pattern.pattern5},
-	{"distance": 3.0, "sprite": Color.GREEN_YELLOW, "pattern": Pattern.pattern6},
+	{"distance": 0.5, "sprite": trash_bag_pine, "pattern": Pattern.pattern5},
+	{"distance": 0.75, "sprite": big_container_texture, "pattern": Pattern.pattern6},
+	{"distance": 1.25, "sprite": old_container, "pattern": Pattern.pattern4},
+	{"distance": 1.75, "sprite": trash_bag_pine, "pattern": Pattern.pattern5},
+	{"distance": 2.0, "sprite": big_container_texture, "pattern": Pattern.pattern6},
+	{"distance": 2.25, "sprite": trash_bag_pine, "pattern": Pattern.pattern5},
+	{"distance": 3.0, "sprite": big_container_texture, "pattern": Pattern.pattern6},
 ]}
 var capitalism_levels = [level1, level2, level3, level4, level5, level6]
 # Otchoz levels
